8eafbdfe71b70032c5a5aa5072f15d622247ca4a
37095@studnetwsei.pl <37095@studnetwsei.pl>
2022-06-08 08:29:19 +0200

    Realizacja punktu 5 I II III oraz podpunktu b

--- src/main/java/pl/lublin/wsei/java/cwiczenia/test/MyDB.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/MyDB.java
@@ -1,4 +1,58 @@
 package pl.lublin.wsei.java.cwiczenia.test;
+        import java.security.KeyPairGenerator;
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.*;
+        import java.util.Properties;
+
+        import com.mysql.jdbc.Driver;
 
 public class MyDB {
+
+    public MyDB(String dbAddress, String dbPort, String dbName) {
+        this.dbAddress = dbAddress;
+        this.dbPort = dbPort;
+        this.dbName = dbName;
+    }
+
+    private Connection conn = null;
+    private String dbAddress;
+    private String dbPort;
+    private String dbName;
+    private String user;
+    private String password;
+
+    private void connect() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.put("serverTimezone", "Europe/Warsaw");
+
+        String jdbcString = "jdbc:mysql://" + dbAddress + ":" + dbPort + "/" + dbName;
+        try {
+            conn = DriverManager.getConnection(
+                    jdbcString, connectionProps);
+            System.out.println("Connected to database" + dbName);
+        } catch (SQLException e) {
+            System.out.println("Błąd połączenia do bazy" + jdbcString);
+            System.out.println("Komunikat błędu:" + e.getMessage());
+            conn = null;
+
+        }
+    }
+    public Connection getConnection () {
+        if (conn == null)
+            connect();
+        return conn;
+    }
+
+    public void setUser (String user){
+        this.user = user;
+    }
+
+    public void setPassword (String password){
+        this.password = password;
+    }
+
+
 }
--- src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestConnection.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestConnection.java
@@ -3,25 +3,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.*;
 import java.util.Properties;
-
+//import MyDB;
 import com.mysql.jdbc.Driver;
 
 public class TestConnection {
         public static void main(String[] args)throws ClassNotFoundException, SQLException{
             String serverName = "localhost";
             String database = "mydb";
-            Number portNumber = 3306;
-            Connection conn = null;
-            Properties connectionProps = new Properties();
-            connectionProps.put("user", "root");
-            connectionProps.put("password", "root");
-            connectionProps.put("serverTimezone", "Europe/Warsaw");
+            String portNumber = "3306";
+//            Number portNumber = 3306;
+//            Connection conn = null;
+//            Properties connectionProps = new Properties();
+//            connectionProps.put("user", "root");
+//            connectionProps.put("password", "root");
+//            connectionProps.put("serverTimezone", "Europe/Warsaw");
+//
+//            conn = DriverManager.getConnection(
+//            "jdbc:mysql://"+ serverName + ":" + portNumber + "/" + database, connectionProps);
+//
+//            System.out.println("Connected to database");
 
-            conn = DriverManager.getConnection(
-            "jdbc:mysql://"+ serverName + ":" + portNumber + "/" + database, connectionProps);
-
-            System.out.println("Connected to database");
-
+            MyDB myDB = new MyDB(serverName, portNumber, database);
+            myDB.setUser("root");
+            myDB.setPassword("root");
+            Connection conn = myDB.getConnection();
+            if (conn != null){
+                System.out.println("Połączono do bazy danych");
+            }else{
+                System.out.println("Problem!!!!");
+            }
 
         }
 }
